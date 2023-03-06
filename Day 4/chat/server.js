42172bca636576829e34776b6366f993f4f632eb
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-03-06 17:25:18 +0200

    add chat app

---
+++ Day 4/chat/server.js
@@ -0,0 +1,24 @@
+const app = require("express")();
+const http = require("http").Server(app);
+
+const io = require("socket.io")(http);
+
+let PORT = process.env.PORT || "7000";
+
+
+app.get('/', function(req, res) {
+  res.render("index.ejs");
+});
+
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit("message",msg);
+  });
+
+});
+
+
+http.listen("7000",()=>{
+  console.log("http://localhost:" + PORT);
+})
